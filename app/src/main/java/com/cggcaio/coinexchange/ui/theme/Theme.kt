aa91e255178e6c726f1e5238396b95f9a0e5e424
caio.goncalves <cggcaio.2014@gmail.com>
2025-02-04 15:13:56 -0300

    Refactor theme system
    - Removes old `Color.kt` and `Type.kt`.
    - Adds custom `CustomColorScheme` and `CustomTypography`.
    - Adds `Color.kt` with color constants.
    - Implements light and dark schemes.
    - Updates `Theme.kt` to use the new theme system.

--- app/src/main/java/com/cggcaio/coinexchange/ui/theme/Theme.kt
+++ app/src/main/java/com/cggcaio/coinexchange/ui/theme/Theme.kt
@@ -1,59 +1,48 @@
 package com.cggcaio.coinexchange.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.Modifier
+import com.cggcaio.coinexchange.ui.theme.AppTheme.color
+import com.cggcaio.coinexchange.ui.theme.AppTheme.typography
+import com.cggcaio.coinexchange.ui.theme.custom.CustomColorScheme
+import com.cggcaio.coinexchange.ui.theme.custom.CustomTypography
+import com.cggcaio.coinexchange.ui.theme.custom.LocalColorScheme
+import com.cggcaio.coinexchange.ui.theme.custom.LocalTypography
+import com.cggcaio.coinexchange.ui.theme.custom.darkScheme
+import com.cggcaio.coinexchange.ui.theme.custom.lightScheme
+import com.cggcaio.coinexchange.ui.theme.custom.toCustomTypography
 
-private val DarkColorScheme =
-    darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80,
-    )
+object AppTheme {
+    val typography: CustomTypography
+        @Composable
+        get() = LocalTypography.current.toCustomTypography()
 
-private val LightColorScheme =
-    lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40,
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
-    )
+    val color: CustomColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorScheme.current
+}
 
 @Composable
 fun CoinExchangeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val colorScheme =
-        when {
-            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-                val context = LocalContext.current
-                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-            }
+    val colorScheme = if (darkTheme) darkScheme() else lightScheme()
 
-            darkTheme -> DarkColorScheme
-            else -> LightColorScheme
-        }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content,
-    )
+    CompositionLocalProvider(
+        LocalColorScheme provides colorScheme,
+        LocalTypography provides typography,
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = color.background,
+            content = content,
+        )
+    }
 }
