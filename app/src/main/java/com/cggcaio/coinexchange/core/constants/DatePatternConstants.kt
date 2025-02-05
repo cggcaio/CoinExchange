0fc37bf5562126bc45cceb9d9b14d339bfa4467f
caio.goncalves <cggcaio.2014@gmail.com>
2025-02-04 21:25:57 -0300

    feat: Add date and number formatting

---
+++ app/src/main/java/com/cggcaio/coinexchange/core/constants/DatePatternConstants.kt
@@ -0,0 +1,6 @@
+package com.cggcaio.coinexchange.core.constants
+
+object DatePatternConstants {
+    const val REQUEST_COMPLETE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+    const val PRESENTATION_DATE_PATTERN = "dd/MM/yyyy"
+}
