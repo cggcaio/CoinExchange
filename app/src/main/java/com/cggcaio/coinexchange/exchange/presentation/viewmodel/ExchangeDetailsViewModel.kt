cfcc38bcd4754b121074e87732bf1bdc30bc03d5
caio.goncalves <cggcaio.2014@gmail.com>
2025-02-08 10:01:55 -0300

    Refactor: Improve error handling and loading state in ExchangeDetailsViewModel

--- app/src/main/java/com/cggcaio/coinexchange/exchange/presentation/viewmodel/ExchangeDetailsViewModel.kt
+++ app/src/main/java/com/cggcaio/coinexchange/exchange/presentation/viewmodel/ExchangeDetailsViewModel.kt
@@ -5,13 +5,18 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cggcaio.coinexchange.core.constants.RequestStatusEnum
+import com.cggcaio.coinexchange.core.constants.RequestStatusEnum.ERROR
+import com.cggcaio.coinexchange.core.constants.RequestStatusEnum.LOADING
+import com.cggcaio.coinexchange.core.constants.RequestStatusEnum.NONE
+import com.cggcaio.coinexchange.core.constants.RequestStatusEnum.SUCCESS
 import com.cggcaio.coinexchange.exchange.domain.model.ExchangeDetails
 import com.cggcaio.coinexchange.exchange.domain.usecase.GetExchangeDetailsUseCase
+import com.cggcaio.coinexchange.network.error.model.CustomNetworkException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,23 +24,37 @@ class ExchangeDetailsViewModel
     @Inject
     constructor(
         private val _getExchangeDetailsUseCase: GetExchangeDetailsUseCase,
-    ) : ViewModel() {
-        private val _exchangeDetails = mutableStateOf<ExchangeDetails?>(null)
-        val exchangeDetails: MutableState<ExchangeDetails?> get() = _exchangeDetails
+    ) : ViewModel(),
+        BaseExchangeDetailsViewModel {
+        private val _exchangeDetails = mutableStateOf<ExchangeDetails?>(value = null)
+        override val exchangeDetails: MutableState<ExchangeDetails?> get() = _exchangeDetails
 
-        private val _loading = mutableStateOf(false)
-        val loading: State<Boolean> get() = _loading
+        private val _error = mutableStateOf<String?>(value = null)
+        override val error: State<String?> get() = _error
 
-        fun getExchangeDetails(id: String) {
-            _loading.value = true
+        private val _requestStatus = mutableStateOf(NONE)
+        override val requestStatus: State<RequestStatusEnum> get() = _requestStatus
+
+        override fun getExchangeDetails(id: String) {
+            _requestStatus.value = LOADING
             _getExchangeDetailsUseCase
                 .produce(params = GetExchangeDetailsUseCase.Params(id = id))
                 .map { details ->
                     _exchangeDetails.value = details
-                }.onCompletion {
-                    _loading.value = false
+                    _requestStatus.value = SUCCESS
                 }.catch { exc ->
-                    val a = exc
+                    if (exc is CustomNetworkException) {
+                        _error.value = exc.friendlyMessage
+                    }
+                    _requestStatus.value = ERROR
                 }.launchIn(viewModelScope)
         }
     }
+
+interface BaseExchangeDetailsViewModel {
+    val exchangeDetails: State<ExchangeDetails?>
+    val requestStatus: State<RequestStatusEnum>
+    val error: State<String?>
+
+    fun getExchangeDetails(id: String)
+}
